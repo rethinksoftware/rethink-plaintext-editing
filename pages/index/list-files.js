f62d9ba0f847de257a28e18040a8ae141e03e0d8
Will Haering <wchaering@users.noreply.github.com>
2020-01-05 21:41:15 -0800

    Fix view overflow

--- pages/index/list-files.js
+++ pages/index/list-files.js
@@ -5,15 +5,17 @@ export function listFiles() {
 
 The primary objective is to extend this application to support plaintext editing. Instead of this default preview, text files will open in an elegant editor built by you. The editor will support writing changes to the file. This objective should not take more than 45 minutes to complete.
 
-All code and assets for this challenge live under pages/index/.
+All code and assets for this challenge live under https://github.com/rethinksoftware/plaintext-editing-challenge.
 
 The editor you'll work on is already passed a File object and a write() function. You'll need to configure write() yourself, but it should update the files list in state. Files do not need to persist between reloads (but it would be neat if they did). Look for REGISTERED_EDITORS in index.js to register editors for different media types.
 
 This challenge is designed to provide unlimited creative freedom. The primary objective is to edit plaintext, but if you're looking for more and want to immediately stand out, feel free to take initiative on secondary objectives of your choosing.
 
-Once complete, email a .zip of clean, tested code to will@rethink.software. It will be run using npm i && npm run dev.
+Once complete, send me (will@rethink.software) a GitHub repository link. I will clone and then run it using npm i && npm run dev.
 
-Thanks for trying our challenge!
+Thanks for trying my challenge!
+
+- Will
 `
     ],
     "/README.txt",

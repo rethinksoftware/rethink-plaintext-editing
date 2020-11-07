42161af00de582e3a9de06d1bf03bb5d24d491f8
mxk5025 <michaelkha213@gmail.com>
2020-11-07 13:46:42 -0500

    Update editors to act like preview

--- components/MarkdownEditor/index.js
+++ components/MarkdownEditor/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import path from 'path';
 
 import css from './style.css';
 
@@ -14,7 +15,8 @@ function MarkdownEditor({ file, write }) {
 
   return (
     <div className={css.editor}>
-      {value}
+      <div className={css.title}>{path.basename(file.name)}</div>
+      <div className={css.content}>{value}</div>
     </div>
   );
 }
--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import path from 'path';
 
 import css from './style.css';
 
@@ -14,7 +15,8 @@ function PlaintextEditor({ file, write }) {
 
   return (
     <div className={css.editor}>
-      {value}
+      <div className={css.title}>{path.basename(file.name)}</div>
+      <div className={css.content}>{value}</div>
     </div>
   );
 }

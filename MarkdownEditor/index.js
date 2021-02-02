4cea0c56aee1308ea729bc9f2b4b0e6e31bcdeca
Adwitiya Singh <adwitiya.karan@gmail.com>
2021-02-02 12:26:00 -0800

    Add real time previews in Editor

--- MarkdownEditor/index.js
+++ MarkdownEditor/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './style.css';
-import PlaintextEditor from '../components/PlaintextEditor';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 
 function MarkdownEditor({ file, write }) {
   console.log(file, write)
@@ -10,24 +10,17 @@ function MarkdownEditor({ file, write }) {
   const ReactMarkdown = require('react-markdown')
   const gfm = require('remark-gfm')
 
-  useEffect(() => {
-    file.text().then((value)=>{
-      setText(value);
-    });
-  }, []);
+  const renderers = {
+    code: ({language, value}) => {
+      return <SyntaxHighlighter language={language} children={value} />
+    }
+  }
 
   useEffect(() => {
     file.text().then((value)=>{
-      if (file.type === "text/javascript" ){
-        value = "```js"+value+"\n```";
-      }
-      if (file.type === "application/json" ){
-        value = "```json\n"+value+"\n```";
-      }
       setText(value);
     });
   }, [file]);
-
 
   let handleTextChange = (event) => {
     setText(event.target.value);
@@ -45,11 +38,22 @@ function MarkdownEditor({ file, write }) {
     write(updatedFile)
   }
 
+  function fileType(value) {
+    if (file.type === "text/javascript" ){
+      value = "```js\n"+value+"\n```";
+    }
+    if (file.type === "application/json" ){
+      value = "```json\n"+value+"\n```";
+    }
+    return value
+  }
+
   return (
     <div className={css.editor}>
+      <h3>{file.name.substring(1)}</h3>
       <div className={css.textArea}>
-        <PlaintextEditor file={file} write={write}></PlaintextEditor>
-        <ReactMarkdown plugins={[gfm]} children={currText} />
+        <textarea onChange={handleTextChange} className={css.textArea} value={currText?currText:""}/>
+        <ReactMarkdown plugins={[gfm]} renderers={renderers} children={fileType(currText)} />
       </div>
     </div>
   );
